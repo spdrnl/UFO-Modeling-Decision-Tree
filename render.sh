00963c8435bf25cf16fefe73baa403489e891b88
sanne <sannederoever@newfoundland.nl>
2026-04-30 17:46:14 +0200

    Fixed render.sh

--- render.sh
+++ render.sh
@@ -1,1 +1,4 @@
-mmdc -i ufo-decision-tree.mermaid -o ufo-decision-tree.png -p <(echo '{"args": ["--no-sandbox", "--disable-setuid-sandbox"]}') -w 3200
+#!/bin/sh
+echo '{"args": ["--no-sandbox", "--disable-setuid-sandbox"]}' > puppeteer-config.json
+mmdc -i ufo-decision-tree.mermaid -o ufo-decision-tree.png -p puppeteer-config.json -w 3200
+rm puppeteer-config.json
