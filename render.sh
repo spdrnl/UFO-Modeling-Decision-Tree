8fe18aa77a0efb6e442d6fc692bebf0e1ae70ab8
sanne <sannederoever@newfoundland.nl>
2026-05-04 17:32:07 +0200

    Separated out the individual decision tree and some other small fixes.

--- render.sh
+++ render.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mmdc -i 01-Start.mermaid -o 01-Start.png -p config.json -s 2 #-w 3200
+mmdc -i 00-Start.mermaid -o 01-Start.png -p config.json -s 2 #-w 3200
+mmdc -i 01-Individual.mermaid -o 01-Individual.png -p config.json -s 2 #-w 3200
 mmdc -i 02-Substantial.mermaid -o 02-Substantial.png -p config.json -s 2 #-w 3200
 mmdc -i 03-Moment.mermaid -o 03-Moment.png -p config.json -s 2 #-w 3200
 mmdc -i 04-Relational.mermaid -o 04-Relational.png -p config.json -s 2 #-w 3200
+mmdc -i 05-Perdurant.mermaid -o 05-Perdurant.png -p config.json -s 2 #-w 3200
